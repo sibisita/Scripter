803de532ffd8ad6adf89323ed43fae88a7d96585
Sibi C B <sitasibi@gmail.com>
2022-03-19 10:52:31 +0530

    Netmiko is implemented. Remote access working

--- main.py
+++ main.py
@@ -6,27 +6,83 @@ from tkinter import filedialog
 from tkinter import scrolledtext
 import os
 import time
+from datetime import datetime
+from tkinter import messagebox
+import subprocess
+from netmiko import Netmiko
 
 root = Tk()
 
 about_this_application = '''
 Hi,
-This application is created for searching a list of IPs from hundreds of network device backup folder. 
-This can be be used for other use cases as well.
+This application is created for taking backup of cisco devices after logging into a jumpserver.
 Source code for this is available in : https://github.com/sibisita/Multi-Search/blob/main/multi_search.py .
 Instructions:
-Step 1: Enter the values you want to search in top left box. Each line is taken a seperate entry.
-Step 2: Select the location where you have the files to be searched. Use 'Browse Search Folder' button.
+Step 1: Enter the cisco device list or IP. Make sure the hostnames entered is correct.
+Step 2: Enter the commands that you want to execute on each host.
 Step 3: Select the location where you want the results to be saved. Use 'Browse Save Folder' button.
-Step 4: (Optional) You can enter any notes in the box below the search button.
-Step 5: Press search button.
-After the search is completed. The results location is automatically opened in windows file explorer.
+Step 4: Enter the Jumpserver and device credentials.
+Step 5: Press Start Execution button.
+After the Execution is completed. The results location is automatically opened in windows file explorer.
 In the application, you can press reset to search again.
 Regards,
 Sibi
 '''
 
 save_location1 = os.path.expanduser('~')
+
+
+def netmiko_logic(devices, commands):
+    logs_entry(
+        f"**** Establishing connection to Jump Server {jumpserver.get()} ****")
+    net_connect = Netmiko(device_type="linux_ssh", host=jumpserver.get(),
+                          username=jumpuser.get(), password=jumppassword.get())
+
+    logs_entry(net_connect.find_prompt())
+    logs_entry("**** Connection to Jump Server Successful ****\n\n")
+
+    logs_entry(output)
+    for device in devices:
+        logs_entry(
+            f"**** Establishing connection to {device} ****")
+        net_connect.write_channel(
+            f'ssh -o "StrictHostKeyChecking no" {deviceuser.get()}@{device}\n')
+        time.sleep(10)
+        output = net_connect.read_channel()
+        logs_entry(output)
+        if "Password" in output:
+            logs_entry("Received password prompt")
+            net_connect.write_channel(f'{devicepassword.get()}\n')
+            logs_entry(
+                f"**** Connection to {device} Successful ****")
+
+
+def main_logic():
+    search.configure(state="disabled")
+
+    start_time = datetime.now()  # Start time to calculate time taken
+
+    # Extracting values from screen
+    device_list = (text_area.get("1.0", "end")).lower().splitlines()
+    device_list_set = list(set(device_list))
+    if "" in device_list_set:
+        device_list_set.remove("")
+    if len(device_list_set) == 0:
+        search.configure(state="normal")
+        messagebox.showinfo("Device list Empty!!", 'Enter list of Devices!!')
+        return
+    command_list = (command_list_area.get("1.0", "end")).lower().splitlines()
+    command_list_set = list(set(command_list))
+    if "" in command_list_set:
+        command_list_set.remove("")
+    if len(command_list_set) == 0:
+        search.configure(state="normal")
+        messagebox.showinfo("Command list Empty!!",
+                            'Enter commands to execute!!')
+        return
+    netmiko_logic(device_list_set, command_list_set)
+    FILEBROWSER_PATH = os.path.join(os.getenv('WINDIR'), 'explorer.exe')
+    subprocess.run([FILEBROWSER_PATH, os.path.normpath(save_location1)])
 
 
 def logs_entry(log, end="\n"):
@@ -40,7 +96,6 @@ def logs_entry(log, end="\n"):
 def new_window():
     window11 = Toplevel(root)
     window11.geometry("1000x400")
-
     window11.title("About This Application!")
     a11 = Text(window11, height=400, width=1000)
     a11.insert(INSERT, about_this_application)
@@ -70,31 +125,33 @@ text_area = scrolledtext.ScrolledText(
 
 l2 = ttk.Label(root, text=save_location1)
 save_loc = ttk.Button(root, text="Browse Save folder", command=save_in_folder)
+
+
 jumpServerLabel = Label(root, text="Jumpserver (name or IP)", font=(
-    "Times New Roman", 12)).grid(row=4, column=3)
+    "Times New Roman", 12))
 jumpserver = StringVar()
-jumpServerEntry = Entry(root, textvariable=jumpserver).grid(row=4, column=4)
+jumpServerEntry = Entry(root, textvariable=jumpserver)
 jumpuserLabel = Label(root, text="Jumpserver Username", font=(
-    "Times New Roman", 12)).grid(row=5, column=3)
+    "Times New Roman", 12))
 jumpuser = StringVar()
-jumpuserEntry = Entry(root, textvariable=jumpuser).grid(row=5, column=4)
+jumpuserEntry = Entry(root, textvariable=jumpuser)
 
-passwordLabel = Label(root, text="Jumpserver Password", font=(
-    "Times New Roman", 12)).grid(row=6, column=3)
-jumpServerpassword = StringVar()
-passwordEntry = Entry(root,  textvariable=jumpServerpassword,
-                      show='*').grid(row=6, column=4)
+jumppasswordLabel = Label(root, text="Jumpserver Password", font=(
+    "Times New Roman", 12))
+jumppassword = StringVar()
+jumppasswordEntry = Entry(root,  textvariable=jumppassword,
+                          show='*')
 
-usernameLabel = Label(root, text="Device Username", font=(
+deviceuserLabel = Label(root, text="Device Username", font=(
     "Times New Roman", 12)).grid(row=10, column=3)
-deviceusername = StringVar()
-usernameEntry = Entry(root, textvariable=deviceusername).grid(row=10, column=4)
+deviceuser = StringVar()
+deviceuserEntry = Entry(root, textvariable=deviceuser).grid(row=10, column=4)
 
-passwordLabel = Label(root, text="Device Password", font=(
+devicepasswordLabel = Label(root, text="Device Password", font=(
     "Times New Roman", 12)).grid(row=11, column=3)
-userpassword = StringVar()
-passwordEntry = Entry(root,  textvariable=userpassword,
-                      show='*').grid(row=11, column=4)
+devicepassword = StringVar()
+devicepasswordEntry = Entry(root,  textvariable=devicepassword,
+                            show='*').grid(row=11, column=4)
 
 enableLabel = Label(root, text="Cisco enable Password", font=(
     "Times New Roman", 12)).grid(row=12, column=3)
@@ -102,7 +159,7 @@ enablepassword = StringVar()
 enablepasswordEntry = Entry(root,  textvariable=enablepassword,
                             show='*').grid(row=12, column=4)
 
-command_list = scrolledtext.ScrolledText(
+command_list_area = scrolledtext.ScrolledText(
     root, wrap=WORD, width=30, height=15, font=("Times New Roman", 12))
 about = ttk.Button(root, text="About", width=8, command=new_window)
 
@@ -117,20 +174,32 @@ output_window = scrolledtext.ScrolledText(
     root, wrap=WORD, width=45, height=30, state='disabled', font=("Times New Roman", 12))
 output_lable = ttk.Label(
     root, text="Searched values and their number of occurances: ", font=("Times New Roman", 10))
+search = Button(root, text="Start Execution", bg="green",
+                fg="yellow", font=100, command=main_logic)
 
 # grid position
 text_area.grid(column=0, row=2, pady=1, padx=1,
                sticky="w", rowspan=15, columnspan=2)
 about.grid(column=4, row=0, padx=5, sticky=N+E)
-command_list.grid(column=1, row=2, pady=1, padx=1,
-                  sticky="w", rowspan=15, columnspan=2)
+command_list_area.grid(column=1, row=2, pady=1, padx=1,
+                       sticky="w", rowspan=15, columnspan=2)
 l2.grid(column=1, row=21, pady=10, padx=5, sticky="w")
 save_loc.grid(column=2, row=21, padx=1, sticky="w")
 space.grid(column=0, row=20, pady=1, padx=1, sticky="w")
-
+search.grid(column=4, row=15)
 file_count.grid(column=0, row=25, pady=10, padx=10)
 statusbar.grid(column=0, row=27, pady=5, padx=5, columnspan=10, sticky=W)
 
+jumpServerLabel.grid(row=4, column=3)
+jumpServerEntry.grid(row=4, column=4)
+jumpuserLabel.grid(row=5, column=3)
+jumpuserEntry.grid(row=5, column=4)
+jumppasswordLabel.grid(row=6, column=3)
+jumppasswordEntry.grid(row=6, column=4)
+
+
 text_area.focus()
 logs_entry("hello")
+
+
 root.mainloop()
